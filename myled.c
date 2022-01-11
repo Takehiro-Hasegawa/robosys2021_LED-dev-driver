9cea573eead704f119d445a389645738b1d46f58
Takehiro Hasegawa <s18C1091KR@s.chibakoudai.jp>
2022-01-11 12:16:45 +0000

    Add a file

---
+++ myled.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2022 Takehiro Hasegawa.
+  
+*/
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+#include <linux/delay.h>
+
+MODULE_AUTHOR("Takehiro Hasegawa and Ryuichi Ueda");
+MODULE_DESCRIPTION("driver for LED control");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.0.1");
+
+static dev_t dev;
+static struct cdev cdv;
+static struct class *cls = NULL;
+
+static volatile u32 *gpio_base = NULL;
+
+static ssize_t led_write(struct file* filp, const char*buf, size_t count, loff_t* pos)
+{
+
+	int second = 600;
+	int i = 1;
+
+	char c;
+	if(copy_from_user(&c, buf, sizeof(char)))
+		return -EFAULT;
+
+	if(c == '0')
+		gpio_base[10] = 1 << 25;
+
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
+
+	else if(c == '2')
+		gpio_base[10] = 1 << 15;
+
+	else if(c == '3')
+		gpio_base[7] = 1 << 15;
+
+	else if(c == '4'){
+
+		printk(KERN_DEFAULT "The train will pass soon.\n");
+
+		while(i <= 46){
+			msleep(second);
+			gpio_base[10] = 1 << 15;
+			gpio_base[7] = 1 << 25;
+			msleep(second);
+			gpio_base[10] = 1 << 25;
+			gpio_base[7] = 1 << 15;
+			
+			i++;
+		}
+		gpio_base[10] = 1 << 15;
+
+		printk(KERN_DEFAULT "The train has passed.\n");
+
+	}
+
+	return 1;
+}
+
+static struct file_operations led_fops = {
+	.owner = THIS_MODULE,
+	.write = led_write
+};
+
+static int __init init_mod(void)
+{
+	int retval;
+
+	gpio_base = ioremap_nocache(0xfe200000, 0xA0);
+
+	const u32 led = 25;
+	const u32 index = led/10;
+	const u32 shift = (led%10)*3;
+	const u32 mask = ~(0x7 << shift);
+	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+
+	const u32 led1 = 15;
+	const u32 index1 = led1/10;
+	const u32 shift1 = (led1%10)*3;
+	const u32 mask1 = ~(0x7 << shift);
+	gpio_base[index1] = (gpio_base[index1] & mask1) | (0x1 << shift1);
+
+	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+	if(retval < 0){
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
+	}
+	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+
+	cdev_init(&cdv, &led_fops);
+	cdv.owner = THIS_MODULE;
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+		return retval;
+	}
+
+	cls = class_create(THIS_MODULE,"myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed.");
+	return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+
+	return 0;
+}
+
+static void __exit cleanup_mod(void)
+{
+	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
+	iounmap(gpio_base);
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
